d2c558493c4a3eaf26b00a488a6cff98ac51d53e
unknown <shresthaalice7@gmail.com>
2020-08-20 19:37:14 +0545

    firstshot

--- lib/main.dart
+++ lib/main.dart
@@ -6,27 +6,14 @@ void main() {
       title: "Hamro Resraurant",
       home: Scaffold(
         drawer: Drawer(),
-        appBar: AppBar(
-          actions: [Icon(Icon.more_vert)],
-          bottom: PreferedSize(
-            child: Container(
-              aligment: Aligment.center,
-              height: 100,
-              width: MediaQuery.of(contex).size.width,
-              child: Column(
-                children:[
-                  Text(
-                    "Hamro Restaurant",
-                    style: TextStyle(color: color(0xfffffff),fontsize: 24),
-                  ),
-                ]
-              ),
-
-            ),
-
-          ),
-        ),
-      ),
-    ),
-  );
+        appBar: AppBar(),
+        ),//AppBar
+        body: ListView(
+          children: [
+            Image.network("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.foodnetwork.com%2Frecipes%2Fkatie-lee%2Fcast-iron-skillet-porterhouse-steak-5190848&psig=AOvVaw2TGbqpslBtwC0dsLMo6oax&ust=1598017788304000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCLD2t6j2qesCFQAAAAAdAAAAABAD"),
+          ]
+        )//ListView
+    )
+  )//scafold
 }
+          
